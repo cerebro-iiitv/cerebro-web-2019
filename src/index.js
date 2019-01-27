9d07af4070a9188af23a10137f7c8bef074ce3d8
Monal5031 <monalshadi16@gmail.com>
2019-01-28 01:22:29 +0530

    Update home

--- src/index.js
+++ src/index.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Icons from 'uikit/dist/js/uikit-icons';
 import UIKit from 'uikit';
+import * as firebase from 'firebase';
 
-import registerServiceWorker from './registerServiceWorker';
-import {getStore} from "./store";
+import { unregister as unregisterServiceWorker } from './serviceWorker';
+import { getState, getStore } from './store';
+
+import 'uikit/dist/css/uikit.min.css';
+
+import Root from './components';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Event from './components/Event';
+import Timeline from './components/Timeline';
+
 
 UIKit.use(Icons);
+
+// Firebase
+firebase.initializeApp({
+	apiKey: 'AIzaSyB22-DZ_PvhTJsA-PPf8Vs--fmcCixL-YQ',
+	authDomain: 'cerebro-2018-f1052.firebaseapp.com',
+	databaseURL: 'https://cerebro-2018-f1052.firebaseio.com',
+	projectId: 'cerebro-2018-f1052',
+	storageBucket: 'cerebro-2018-f1052.appspot.com',
+	messagingSenderId: '1059299836137'
+});
 
 const store = getStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-        <div>
-            Hello World!
-        </div>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={Root} />
+					<Route
+						exact
+						path="/dashboard"
+						render={() => {
+							if (getState().auth.user) {
+								return <Dashboard />;
+							} else {
+								return <Redirect to="/" />;
+							}
+						}}
+					/>
+					<Route exact path={'/timeline'} component={Timeline} />
+					<Route exact path={'/events/:id'} component={Event} />
+					<Route exact path={'/about'} component={About} />
+				</Switch>
+				<Footer />
+			</div>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
 );
 
-serviceWorker.unregister();
+unregisterServiceWorker();
