5cbb9b89f4422a97276723fee99ee77830e4bb41
Jay Mistry <mistry.jay15@gmail.com>
2019-02-18 23:38:05 +0530

    Add particles bg (#32)
    * animated background
    
    * animated background
    
    * increase density:

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import EventsList from '../components/EventsList';
 import Logo from '../img/anshumanvisallstackdeveloper.png';
+import ParticlesBg from './ParticlesBg'
 
 const HomePage = () => (
 	<main className="homepage">
 		<div className="homepage__logo-wrapper">
+			<ParticlesBg id='particles-bg' />
 			<img src={Logo} alt="Logo" className="homepage__logo" />
 		</div>
 		<EventsList />
