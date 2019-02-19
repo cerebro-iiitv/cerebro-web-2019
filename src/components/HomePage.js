180a59f0f781198b8c46c5906b9f845f7a30189b
neeraj3029 <31539812+neeraj3029@users.noreply.github.com>
2019-02-20 02:35:47 +0530

    scrolldown button added (#51)

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,11 +3,24 @@ import EventsList from '../components/EventsList';
 import Logo from '../img/anshumanvisallstackdeveloper.png';
 import ParticlesBg from './ParticlesBg'
 
+const scrollDown = () => {
+	window.scrollTo({
+		top: 780,
+		left: 0,
+		behavior: 'smooth'
+	});
+}
+
 const HomePage = () => (
 	<main className="homepage">
 		<div className="homepage__logo-wrapper">
 			<ParticlesBg id='particles-bg' />
 			<img src={Logo} alt="Logo" className="homepage__logo" />
+		</div>
++       <div className="scrollButton" onClick = {scrollDown}>
++       	<span className = "scrollLink">
+				<img alt="Arrow Down Icon" src="https://www.solodev.com/assets/anchor/arrow-down.png"/>
+			</span>
 		</div>
 		<EventsList />
 	</main>
